576aabb976282d5f0540a20041bb24d516cb7400
Naeem-pro <90679573+Naeem-pro@users.noreply.github.com>
2022-01-23 13:16:53 +0500

    Callback functions

--- functions.js
+++ functions.js
@@ -18,18 +18,48 @@
 
 const students = [
   { name: "Naeem", profession: "stundent" },
-  { name: "Naeem1", profession: "student" },
+  { name: "Naeem1", profession: "student1" },
 ];
-// console.log(students[0].name); //Access the name of first object of array means of zero index
 
-// for (let index = 0; index < students.length; index++) {
-//   console.log(students[index].name + " " + students[index].profession);
+// function enrollStudents(student) {
+//   setTimeout(() => {
+//     students.push(student); //This line of code will execute after 2 seconds
+//     console.log("New student has been added successfully");
+//   }, 2000);
 // }
 
-function enrollStudents() {
+// function getStudents() {
+//   setTimeout(() => {
+//     students.forEach((student, index) => {
+//       //This block of code will execute after 1 seconds
+//       console.log(student.name);
+//     });
+//   }, 1000);
+// }
+
+// enrollStudents({ name: "Naeem3", profession: "student" });
+// getStudents();
+
+//Before using callback function, getStudents() was executed earlier than enrollStudents() beacuse the time to get execute for getStudents() is 1 second and and for enrollStudents() 2 seconds. As a result the data of existing students printed first and new student enroll later. So the name of new enrolled student is not accessible. Uncomment the above code(functions) and check result
+//Inorder to solve this problem we use callback fuctions();
+//Let's use callback function, above code will look like
+
+function enrollStudents(student, callback) {
   setTimeout(() => {
-    console.log("enroll functions");
-  }, 2000);
+    students.push(student);
+    console.log("New student has been added successfully");
+    callback(); //In this case we are saying that after adding new student, fire the callback functions and execute that function
+  }, 3000);
 }
 
-enrollStudents();
+function getStudents() {
+  setTimeout(() => {
+    students.forEach((student, index) => {
+      //This block of code will execute after 1 seconds
+      console.log(student.name + " " + student.profession);
+    });
+  }, 1000);
+}
+
+console.log("Using callback function");
+enrollStudents({ name: "Naeem2", profession: "student2" }, getStudents);
